fefca86799104699e08e61055957f403078c24c4
William GT <willygt@outlook.com>
2015-11-18 13:05:36 -0800

    Acceso a variable estatica asincronica

--- Testing/Testing/Program.cs
+++ Testing/Testing/Program.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Testing
 {
     class Program
     {
+        [ThreadStatic]
+        static int _field;
+
         static void Main(string[] args)
         {
+            new Thread(() =>
+            {
+                for(int i =0; i<10; i++)
+                {
+                    Console.WriteLine("Thread A: " + _field++);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Thread B: " + _field++);
+                }
+            }).Start();
+
+            Console.Read();
+
         }
     }
 }
